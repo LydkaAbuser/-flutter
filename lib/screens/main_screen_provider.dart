651806af408147ea936312ce9172fd9fbd7a98b5
LydkaAbuser <deadsmilezx@gmail.com>
2025-11-26 20:20:46 +0900

    Лаб  5

---
+++ lib/screens/main_screen_provider.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'cubit/main_screen_cubit.dart';
+import 'main_screen.dart';
+
+class MainScreenProvider extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => MainScreenCubit(),
+      child: MainScreen(),
+    );
+  }
+}
